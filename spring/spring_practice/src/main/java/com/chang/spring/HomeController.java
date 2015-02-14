a878bd53098f96c6472739869941b3d9463f2604
cheochangwon <cheochangwon@cheochangwon-ui-MacBook-Pro.local>
2015-02-15 00:58:34 +0900

    1)remove target

--- spring/spring_practice/src/main/java/com/chang/spring/HomeController.java
+++ spring/spring_practice/src/main/java/com/chang/spring/HomeController.java
@@ -22,7 +22,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -35,7 +35,7 @@ public class HomeController {
 		
 		return "home";
 	}
-	@RequestMapping(value = "/home2", method = RequestMethod.GET)
+	@RequestMapping(value = "/home2.do", method = RequestMethod.GET)
 	public String home2(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
